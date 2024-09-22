97caedeb930945adbbc57a85c60ffbc59095cb10
gpcosmic <161995549+gpcosmic@users.noreply.github.com>
2024-09-22 14:08:02 -0400

    Add debug print messages as well as building all content into a variable.

--- src/Engine/PngFile.java
+++ src/Engine/PngFile.java
@@ -7,15 +7,17 @@ import Utils.Constants;
 import Utils.HandyFunctions;
 import org.joou.UByte;
 import org.joou.UInteger;
+import org.joou.UNumber;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 public class PngFile {
     final UByte[] headerBytes;
-    Ihdr ihdr;
-    Idat idat;
-    Iend iend;
+    private Ihdr ihdr;
+    private Idat idat;
+    private Iend iend;
 
     public PngFile(long width, long height, short bitDepth, short colorType, short compressionMethod, short filterMethod, short interlaceMethod) {
         this.headerBytes = Constants.HEADER_SIGNATURE;
@@ -28,6 +30,15 @@ public class PngFile {
         }
     }
 
+    public ArrayList<UNumber> allAllContent() {
+        ArrayList<UNumber> allAllContentLol = new ArrayList<>();
+        allAllContentLol.addAll(List.of(headerBytes));
+        allAllContentLol.addAll(ihdr.getAllContent());
+        allAllContentLol.addAll(idat.getAllContent());
+        allAllContentLol.addAll(iend.getAllContent());
+        return allAllContentLol;
+    }
+
     public void printData() {
         // Output
         StringBuilder outputSb = new StringBuilder();
@@ -37,15 +48,15 @@ public class PngFile {
         System.out.printf("Header Signature: %s\n", printHelperBrackets(headerSb, true));
 
         // IHDR
-        StringBuilder ihdrSb = printHelperBuilder(this.ihdr.getData());
+        StringBuilder ihdrSb = printHelperBuilder(this.ihdr.getAllContent());
         System.out.printf("IHDR: %s\n", printHelperBrackets(ihdrSb, true));
 
         // IDAT
-        StringBuilder idatSb = printHelperBuilder(this.idat.getData());
+        StringBuilder idatSb = printHelperBuilder(this.idat.getAllContent());
         System.out.printf("IDAT: %s\n", printHelperBrackets(idatSb, true));
 
         // IEND
-        StringBuilder iendSb = printHelperBuilder(this.iend.getData());
+        StringBuilder iendSb = printHelperBuilder(this.iend.getAllContent());
         System.out.printf("IEND: %s\n", printHelperBrackets(iendSb, true));
 
         outputSb.append(headerSb);
@@ -55,6 +66,13 @@ public class PngFile {
         System.out.printf("Final Stream:\n>>> %s\n", printHelperBrackets(outputSb, false));
     }
 
+    /**
+     * Imprime en consola un String encerrado entre corchetes.
+     *
+     * @param string      String a encerrar entre corchetes.
+     * @param blankSpaces Elimina los espacios en blanco que tenga el string por parámetros.
+     * @return El string pasado por parámetros encerrado entre corchetes.
+     */
     private StringBuilder printHelperBrackets(StringBuilder string, boolean blankSpaces) {
         StringBuilder output = new StringBuilder(string);
         if (!blankSpaces) {
@@ -68,6 +86,14 @@ public class PngFile {
         return new StringBuilder("[" + output + "]");
     }
 
+    /**
+     * Construye un StringBuilder con los datos que contiene el objeto iterable. Usado para representar
+     * en base 16 los bytes que estén contenidos en un array.
+     *
+     * @param iterable Iterable. Debe ser un array de bytes (UByte) o de enteros (UInteger), pero se puede extender
+     *                 para otros tipos de datos numéricos iterables.
+     * @return StringBuilder con la representación de un arreglo o lista de bytes o enteros separados por espacios.
+     */
     private StringBuilder printHelperBuilder(Iterable<?> iterable) {
         // Super cursed Iterator, pero funciona!
         Iterator<?> iterator = iterable.iterator();

1c9952ce8e39016dab7988ef7edfd044c4632ce6
gpcosmic <161995549+gpcosmic@users.noreply.github.com>
2024-09-22 14:08:38 -0400

    Add creating binary file feature.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,11 @@
 import Engine.PngFile;
+import org.joou.UByte;
+import org.joou.UInteger;
+import org.joou.UNumber;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,9 +17,23 @@ public class Main {
         short filterMethod = 0;
         short interlaceMethod = 0;
 
+        String imgPath = "Test.png";
+
         try {
             PngFile pngFile = new PngFile(width, height, bitDepth, colorType, compressionMethod, filterMethod, interlaceMethod);
             pngFile.printData();
+            try (FileOutputStream img = new FileOutputStream(imgPath)) {
+                for (UNumber uN : pngFile.allAllContent()) {
+                    if (uN instanceof UByte) {
+                        img.write(uN.byteValue());
+                    } else if (uN instanceof UInteger) {
+                        byte[] intBytes = ByteBuffer.allocate(4).putInt(uN.intValue()).array();
+                        img.write(intBytes);
+                    }
+                }
+            } catch (IOException ioe) {
+                ioe.printStackTrace();
+            }
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
